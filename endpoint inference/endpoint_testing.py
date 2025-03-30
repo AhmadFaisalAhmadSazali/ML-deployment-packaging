64d9999f54f8b75c5e1b0dc374d0dbde43d809f8
AhmadFaisalAhmadSazali <faisalsazali@gmail.com>
2025-03-30 11:53:26 +0800

    reorganize files

--- endpoint inference/endpoint_testing.py
+++ endpoint inference/endpoint_testing.py
@@ -4,7 +4,7 @@ import requests
 
 url = "http://127.0.0.1:5000/prediction"
 
-data = pd.read_csv("data/raw/diabetes_binary_health_indicators_BRFSS2015.csv")
+data = pd.read_csv("../data/raw/diabetes_binary_health_indicators_BRFSS2015.csv")
 data.columns = data.columns.str.lower()
 features = data.drop(columns=["diabetes_binary"])
 
@@ -12,10 +12,8 @@ ten_samples = features.sample(10)
 ten_samples_dict = ten_samples.to_dict(orient="index")
 
 payload = {"data": ten_samples_dict}
-ten_samples_dict
 
 response = requests.post(url, json=payload)
-response
 
 if response.status_code == 200:
     predictions = response.json()
